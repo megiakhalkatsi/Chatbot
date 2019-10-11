9cf9bc424582a80f02eab1cfaea7257fcaec16c9
Lekiniu <34183251+Lekiniu@users.noreply.github.com>
2019-10-11 18:06:55 +0400

    commit by ika

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-/* hideChat(3); */
+
 
 $('#prime').click(function() {
   toggleFab();
@@ -18,21 +18,7 @@ function toggleFab() {
   
 }
 
- /*  $('#chat_first_screen').click(function(e) {
-        hideChat(1);
-  });
 
-  $('#chat_second_screen').click(function(e) {
-        hideChat(2);
-  });
-
-  $('#chat_third_screen').click(function(e) {
-        hideChat(3);
-  });
-
-  $('#chat_fourth_screen').click(function(e) {
-        hideChat(4);
-  }); */
 
   $('#chat_fullscreen_loader').click(function(e) {
       $('.fullscreen').toggleClass('zmdi-window-maximize');
@@ -44,54 +30,9 @@ function toggleFab() {
       $('.chat_header').toggleClass('chat_header2');
       $('.fab_field').toggleClass('fab_field2');
       $('.chat_converse').toggleClass('chat_converse2');
-      //$('#chat_converse').css('display', 'none');
-     // $('#chat_body').css('display', 'none');
-     // $('#chat_form').css('display', 'none');
-     // $('.chat_login').css('display', 'none');
-     // $('#chat_fullscreen').css('display', 'block');
+      
   });
 
-/* function hideChat(hide) {
-    switch (hide) {
-      case 0:
-            $('#chat_converse').css('display', 'none');
-            $('#chat_body').css('display', 'none');
-            $('#chat_form').css('display', 'none');
-            $('.chat_login').css('display', 'block');
-            $('.chat_fullscreen_loader').css('display', 'none');
-             $('#chat_fullscreen').css('display', 'none');
-            break;
-      case 1:
-            $('#chat_converse').css('display', 'block');
-            $('#chat_body').css('display', 'none');
-            $('#chat_form').css('display', 'none');
-            $('.chat_login').css('display', 'none');
-            $('.chat_fullscreen_loader').css('display', 'block');
-            break;
-      case 2:
-            $('#chat_converse').css('display', 'none');
-            $('#chat_body').css('display', 'block');
-            $('#chat_form').css('display', 'none');
-            $('.chat_login').css('display', 'none');
-            $('.chat_fullscreen_loader').css('display', 'block');
-            break;
-      case 3:
-            $('#chat_converse').css('display', 'none');
-            $('#chat_body').css('display', 'none');
-            $('#chat_form').css('display', 'block');
-            $('.chat_login').css('display', 'none');
-            $('.chat_fullscreen_loader').css('display', 'block');
-            break;
-      case 4:
-            $('#chat_converse').css('display', 'none');
-            $('#chat_body').css('display', 'none');
-            $('#chat_form').css('display', 'none');
-            $('.chat_login').css('display', 'none');
-            $('.chat_fullscreen_loader').css('display', 'block');
-            $('#chat_fullscreen').css('display', 'block');
-            break;
-    }
-}  */
 
 function showchat(){     
             $('#chat_converse').css('display', 'none');
